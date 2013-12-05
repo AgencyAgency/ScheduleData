ea1f5099c12c62fdcd6ce29e9c9bd37fdfed50ca
Kyle Oba <koba@mudphone.com>
2013-12-05 09:22:51 -1000

    Simple parsing of converted tab-delimited file (now UTF-8).

---
+++ parse.rb
@@ -0,0 +1,10 @@
+require "csv"
+DATA_DIR="data"
+# DATA_FILE="#{DATA_DIR}/faculty 2013-2014 Cycles and Schedules 112213.txt"
+DATA_FILE="#{DATA_DIR}/converted.txt"
+options = { col_sep: "\t",
+            quote_char: "~" }
+parsed_file = CSV.read(DATA_FILE, options)
+parsed_file.each do |line|
+  p line
+end
