3b911fe29b72f06ecf64d3da41acc96cd22f123c
Kyle Oba <koba@mudphone.com>
2013-12-05 09:38:08 -1000

    Add basic Rails app to handle data input.

---
+++ HornetsNest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HornetsNest::Application.config.secret_key_base = '749bf55a98bbc79a50ad949ed34cd967b7a91a62b91819b3172876d00710526d003c0019e275cf6293b17c0ca9869f520cef1d96fc1a9d3dcf2da519b71a41ea'
---
+++ HornetsNest/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HornetsNest::Application.config.session_store :cookie_store, key: '_HornetsNest_session'
