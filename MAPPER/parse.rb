8b118cfe434760bb1cd10b77c09ac9fad866e223
Kyle Oba <koba@mudphone.com>
2013-12-05 19:08:52 -1000

    Works! Allows loading bell schedules from data file.

--- MAPPER/parse.rb
+++ MAPPER/parse.rb
@@ -1,4 +1,31 @@
 require 'csv'
+require 'sqlite3'
+require 'data_mapper' # requires all the gems listed above
+
+unless LOG_DIRECTORY
+  LOG_DIRECTORY = "log"
+  DB_DIRECTORY = "db"
+  DB_PATH = "#{DB_DIRECTORY}/hacked_schedule.sqlite"
+  DB_CONN_STR = "sqlite://#{Dir.pwd}/#{DB_PATH}"
+end
+
+# DATA MAPPER
+# If you want the logs displayed you have to do this before the call to setup
+# DataMapper::Logger.new($stdout, :debug)
+DataMapper::Logger.new("#{LOG_DIRECTORY}/data_mapper.log", :debug)
+DataMapper::Model.raise_on_save_failure = true
+
+DataMapper.setup(:default, DB_CONN_STR)
+
+class Schedule
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :day, Date
+  property :cycle, Integer
+  property :title, String
+end
+DataMapper.auto_upgrade!
 
 def parse_schedule raw_schedule
   options = { col_sep: "\t",
@@ -13,15 +40,39 @@ def parse_schedule raw_schedule
 end
 
 def hash_schedule raw_schedule
-  schedule = []
+  all = []
   options = { headers: true, 
               header_converters: :symbol,
               converters: :all,
               col_sep: "\t",
-              quote_char: "~"
-            }
+              quote_char: "~" }
   CSV.foreach(raw_schedule, options) do |row|
-    schedule << Hash[row.headers[0..-1].zip(row.fields[0..-1])]
+    all << Hash[row.headers[0..-1].zip(row.fields[0..-1])]
   end
-  return schedule
+  return all
+end
+
+def load_schedule_from hashed_day
+  h = hashed_day
+
+  start_date = h[:start_date]
+  date = Date.strptime start_date, "%m/%d/%Y"
+  title = h[:title].strip.gsub(/[^a-zA-Z0-9 ]/, "")
+  schedule = Schedule.new( day: date,
+                cycle: h[:cycle].to_i,
+                title: title)
+  unless schedule.save
+    puts "Error saving schedule!\n#{h}"
+  end
+end
+
+def bell_schedules_from hashed_schedule
+  hashed_schedule.select { |day| day[:type] != "Cycle" }
+end
+
+def load_bell_schedules_from hashed_schedule
+  bells = bell_schedules_from hashed_schedule
+  bells.each do |day|
+    load_schedule_from day
+  end
 end
