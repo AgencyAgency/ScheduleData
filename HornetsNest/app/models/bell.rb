1f7c00bb59e25c4e9661ce6bd3ec19e10a76cfe3
Kyle Oba <koba@mudphone.com>
2014-01-21 03:15:56 -1000

    Allow lookup of school days for particular bell names.

--- HornetsNest/app/models/bell.rb
+++ HornetsNest/app/models/bell.rb
@@ -2,7 +2,23 @@ class Bell < ActiveRecord::Base
   has_many :bell_cycles
   has_many :cycles, through: :bell_cycles
 
+  def school_days
+    bell_cycles.inject([]) { |mem, bell_cycle| mem.concat bell_cycle.school_days }
+  end
+
   class << self
+
+    def school_days_for_bell_name bell_name
+      bell = find_by_name bell_name
+      bell.school_days
+    end
+
+    def school_days_for_bell_names *bell_names_array
+      bell_names_array.flatten.inject([]) do |mem, bell_name|
+        mem.concat(school_days_for_bell_name(bell_name))
+      end
+    end
+
     def find_or_create_by_name name
       match = find_all_by_name name
       bell = nil
